88e3dc640adc96873e06e886589988af3d475d57
mingkun.li <mingkun.li@shopee.com>
2024-04-10 12:16:16 +0800

    [L-035] Learn hooks

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -2,32 +2,35 @@ import { useEffect, useState } from 'react';
 
 // ----------------------------------------------------------------------
 
+// Learn hooks
 export default function useLocalStorage(key, defaultValue) {
+  // useState惰性初始化函数和更新函数 https://juejin.cn/post/7022908934235095054
   const [value, setValue] = useState(() => {
-    const storedValue = localStorage.getItem(key);
-    return storedValue === null ? defaultValue : JSON.parse(storedValue);
-  });
+    const storedValue = localStorage.getItem(key)
+    return storedValue === null ? defaultValue : JSON.parse(storedValue)
+  })
 
   useEffect(() => {
     const listener = (e) => {
       if (e.storageArea === localStorage && e.key === key) {
-        setValue(JSON.parse(e.newValue));
+        setValue(JSON.parse(e.newValue))
       }
-    };
-    window.addEventListener('storage', listener);
+    }
+    window.addEventListener('storage', listener)
 
     return () => {
-      window.removeEventListener('storage', listener);
-    };
-  }, [key, defaultValue]);
+      window.removeEventListener('storage', listener)
+    }
+  }, [key, defaultValue])
 
+  // 使用函数作为setValue会导致每次渲染都会执行,而不是多次点击只执行一次
   const setValueInLocalStorage = (newValue) => {
     setValue((currentValue) => {
-      const result = typeof newValue === 'function' ? newValue(currentValue) : newValue;
-      localStorage.setItem(key, JSON.stringify(result));
-      return result;
-    });
-  };
+      const result = typeof newValue === 'function' ? newValue(currentValue) : newValue
+      localStorage.setItem(key, JSON.stringify(result))
+      return result
+    })
+  }
 
-  return [value, setValueInLocalStorage];
+  return [value, setValueInLocalStorage]
 }
