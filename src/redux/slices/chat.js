192abf0899d183dbce807aaea868dcaa5d4ee945
mingkun.li <mingkun.li@shopee.com>
2024-05-09 18:49:37 +0800

    [D-48] Merge remote msg with local msg

--- src/redux/slices/chat.js
+++ src/redux/slices/chat.js
@@ -23,50 +23,89 @@ const initialState = {
   recipients: [],
 };
 
+const mergeAndSortConversation = (localConversation, remoteConversation) => {
+  const localMessages = localConversation.messages
+  const remoteMessages = remoteConversation.messages
+  // 合并两个数组
+  const mergedMessages = [...localMessages, ...remoteMessages]
+
+  // 使用reduce方法来创建一个映射对象，键为id，值为消息对象
+  const messagesMap = mergedMessages.reduce((acc, message) => {
+    const msgId = message.id
+    acc[msgId] = message
+    return acc
+  }, {})
+
+  // 将映射对象转换为数组，并使用Object.values来获取值（即消息对象）
+  const uniqueMessages = Object.values(messagesMap)
+
+  // 根据createdAt属性进行排序，如果createdAt相同，则保持原顺序（稳定排序）
+  uniqueMessages.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt))
+
+  return {
+    ...localConversation,
+    messages: uniqueMessages,
+  }
+}
+
 const slice = createSlice({
   name: 'chat',
   initialState,
   reducers: {
     // START LOADING
     startLoading(state) {
-      state.isLoading = true;
+      state.isLoading = true
     },
 
     // HAS ERROR
     hasError(state, action) {
-      state.isLoading = false;
-      state.error = action.payload;
+      state.isLoading = false
+      state.error = action.payload
     },
 
     // GET CONTACT SSUCCESS
     getContactsSuccess(state, action) {
-      const contacts = action.payload;
+      const contacts = action.payload
 
-      state.contacts.byId = objFromArray(contacts);
-      state.contacts.allIds = Object.keys(state.contacts.byId);
+      state.contacts.byId = objFromArray(contacts)
+      state.contacts.allIds = Object.keys(state.contacts.byId)
     },
 
     // GET CONVERSATIONS
     getConversationsSuccess(state, action) {
-      const conversations = action.payload;
+      const conversations = action.payload
+      // Merge with existing conversations
 
-      state.conversations.byId = objFromArray(conversations);
-      state.conversations.allIds = Object.keys(state.conversations.byId);
+      conversations.forEach((conversation) => {
+        if (!state.conversations.allIds.includes(conversation.id)) {
+          state.conversations.byId[conversation.id] = conversation
+          state.conversations.allIds.push(conversation.id)
+        } else {
+          // Merge with existing conversation
+          state.conversations.byId[conversation.id] = mergeAndSortConversation(
+            state.conversations.byId[conversation.id],
+            conversation,
+          )
+        }
+      })
     },
 
     // GET CONVERSATION
     getConversationSuccess(state, action) {
-      const conversation = action.payload;
-
+      const conversation = action.payload
       if (conversation) {
-        state.activeConversationId = conversation.id
         if (!state.conversations.allIds.includes(conversation.id)) {
           state.conversations.allIds.push(conversation.id)
           state.conversations.byId[conversation.id] = conversation
         } else {
-          // TODO: Merge with existing conversation
-          // state.conversations.byId[conversation.id] = conversation;
+          // Merge with existing conversation
+          const mergedConversation = mergeAndSortConversation(
+            state.conversations.byId[conversation.id],
+            conversation,
+          )
+          state.conversations.byId[conversation.id] = mergedConversation
         }
+        state.activeConversationId = conversation.id
       } else {
         state.activeConversationId = null
       }
@@ -74,8 +113,17 @@ const slice = createSlice({
 
     // ON SEND MESSAGE
     onSendMessage(state, action) {
-      const conversationMsg = action.payload;
-      const { conversationId, messageId, error, message, contentType, attachments, createdAt, senderId } = conversationMsg;
+      const conversationMsg = action.payload
+      const {
+        conversationId,
+        messageId,
+        error,
+        message,
+        contentType,
+        attachments,
+        createdAt,
+        senderId,
+      } = conversationMsg
 
       const newMessage = {
         id: messageId,
@@ -85,15 +133,24 @@ const slice = createSlice({
         attachments,
         createdAt,
         senderId,
-      };
+      }
 
-      state.conversations.byId[conversationId].messages.push(newMessage);
+      state.conversations.byId[conversationId].messages.push(newMessage)
     },
 
     // ON UPDATE MESSAGE
     onUpdateMessage(state, action) {
-      const conversationMsg = action.payload;
-      const { conversationId, messageId, error, message, contentType, attachments, createdAt, senderId } = conversationMsg;
+      const conversationMsg = action.payload
+      const {
+        conversationId,
+        messageId,
+        error,
+        message,
+        contentType,
+        attachments,
+        createdAt,
+        senderId,
+      } = conversationMsg
 
       const newMessage = {
         id: messageId,
@@ -103,47 +160,47 @@ const slice = createSlice({
         attachments,
         createdAt,
         senderId,
-      };
-      let updated = false;
-      state.conversations.byId[conversationId].messages = state.conversations.byId[conversationId].messages.map(
-        (message) => {
-          if (message.id === messageId) {
-            updated = true;
-            return newMessage;
-          }
-          return message;
+      }
+      let updated = false
+      state.conversations.byId[conversationId].messages = state.conversations.byId[
+        conversationId
+      ].messages.map((message) => {
+        if (message.id === messageId) {
+          updated = true
+          return newMessage
         }
-      );
+        return message
+      })
       if (!updated) {
-        state.conversations.byId[conversationId].messages.push(newMessage);
+        state.conversations.byId[conversationId].messages.push(newMessage)
       }
     },
 
     markConversationAsReadSuccess(state, action) {
-      const { conversationId } = action.payload;
-      const conversation = state.conversations.byId[conversationId];
+      const { conversationId } = action.payload
+      const conversation = state.conversations.byId[conversationId]
       if (conversation) {
-        conversation.unreadCount = 0;
+        conversation.unreadCount = 0
       }
     },
 
     // GET PARTICIPANTS
     getParticipantsSuccess(state, action) {
-      const participants = action.payload;
-      state.participants = participants;
+      const participants = action.payload
+      state.participants = participants
     },
 
     // RESET ACTIVE CONVERSATION
     resetActiveConversation(state) {
-      state.activeConversationId = null;
+      state.activeConversationId = null
     },
 
     addRecipients(state, action) {
-      const recipients = action.payload;
-      state.recipients = recipients;
+      const recipients = action.payload
+      state.recipients = recipients
     },
   },
-});
+})
 
 // Reducer
 export default slice.reducer;
